635cbfbc8f31e42f647e2fe392f12652abc60411
Student <example@example.com>
2019-05-20 10:39:26 -0700

    Add properties and getters/setters for Hike class

--- TakeAHike/Models/Hike.cs
+++ TakeAHike/Models/Hike.cs
@@ -1,10 +1,55 @@
 using System.Collections.Generic;
 using MySql.Data.MySqlClient;
 
-namespace HairSalon.Models
+namespace TakeAHike.Models
 {
   public class Hike
   {
+    private string _name;
+    private int _difficulty;
+    private float _distance;
+    private int _id;
+
+    public Client (string name, int difficulty, float distance, int id = 0)
+    {
+      _name = name;
+      _difficulty = difficulty;
+      _distance = distance;
+      _id = id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public int GetDifficulty()
+    {
+      return _difficulty;
+    }
+
+    public void SetDifficulty( int newDifficulty)
+    {
+      _difficulty = newDifficulty;
+    }
+
+    public float GetDistance()
+    {
+      return _distance;
+    }
+
+    public void SetDistance(float newDistance)
+    {
+      _distance = newDistance;
+    }
+
+
+
 
   }
 }
