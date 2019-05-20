bb12459ce0c739d6616be4367f81fb73e54f2d0d
Student <example@example.com>
2019-05-20 11:02:57 -0700

    Change 'hike' class name to 'trail'

--- TakeAHike/Models/Feature.cs
+++ TakeAHike/Models/Feature.cs
@@ -6,6 +6,10 @@ namespace TakeAHike.Models
   public class Feature
   {
     private string _description;
+    private bool _waterfalls;
+    private bool _summits;
+    private bool _wildlife;
+    private bool _dogs;
     private int _id;
 
     public Feature(string description, int id = 0)
@@ -20,8 +24,44 @@ namespace TakeAHike.Models
     }
     public void SetDescription(string newDescription)
     {
-      newDescription = _description;
+      _description = newDescription;
     }
 
+    public bool GetWaterfalls()
+    {
+      return _waterfalls;
+    }
+    public void SetWaterfalls(bool hasWaterfalls)
+    {
+      _waterfalls = hasWaterfalls;
+    }
+
+    public bool GetSummits()
+    {
+      return _summits;
+    }
+    public void SetSummits(bool hasSummits)
+    {
+      _summits = hasSummits;
+    }
+
+    public bool GetWildlife()
+    {
+      return _wildlife;
+    }
+    public void SetWildlife(bool hasWildlife)
+    {
+      _wildlife = hasWildlife;
+    }
+
+    public bool GetDogs()
+    {
+      return _dogs;
+    }
+    public void SetDogs(bool isDogFriendly)
+    {
+      _dogs = isDogFriendly;
+    }
+    
   }
 }
